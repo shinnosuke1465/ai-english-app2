fb21c41bf9279ef9f5e5b06048937a0eab0e5d9c
Shibayama Sho <78073669+shibayamasho@users.noreply.github.com>
2024-09-29 22:06:42 +0900

    画面デザイン作成 トップ画面設置

---
+++ resources/js/Pages/Top.jsx
@@ -0,0 +1,10 @@
+import { Head } from '@inertiajs/react'
+
+export default function Top({}) {
+    return (
+        <>
+            <Head title="Top" />
+            <h1>AI英会話アプリのトップページです</h1>
+        </>
+    )
+}
