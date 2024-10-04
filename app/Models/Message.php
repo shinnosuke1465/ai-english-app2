ec719b182bd305576fa03849eb63000b35c73a21
Shibayama Sho <78073669+shibayamasho@users.noreply.github.com>
2024-10-04 23:24:49 +0900

    モデルにfillableとリレーションを追記

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    // フィラブルプロパティの追加
+    protected $fillable = [
+        'thread_id',
+        'message_en',
+        'message_ja',
+        'sender',
+        'audio_file_path'
+    ];
+
+    // threadメソッドを追加
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
 }
--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     use HasFactory;
+
+    // フィラブルプロパティの追加
+    protected $fillable = ['title'];
+
+    // messagesメソッドを追加
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
